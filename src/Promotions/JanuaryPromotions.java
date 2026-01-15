46d28b6d23f54a434db051c6701eb3eb6b9c2a83
Sum <msc.mars92@gmail.com>
2026-01-15 16:03:43 +0000

    january promo
    January promotion3
    
    January promotion7
    
    January9
    
    January4

--- src/Promotions/JanuaryPromotions.java
+++ src/Promotions/JanuaryPromotions.java
@@ -5,6 +5,13 @@ public class JanuaryPromotions {
         System.out.println("January Promotions");
         System.out.println("123");
         System.out.println("January1");
-        System.out.println("January2");
+        System.out.println("January3");
+        System.out.println("January4");
+        System.out.println("January5");
+        System.out.println("January6");
+        System.out.println("January8");
+        System.out.println("January7");
+        System.out.println("January9:");
+
     }
 }
